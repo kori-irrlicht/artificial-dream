a346d656db94f116652dee5e189e65d9caeb7016
kori-irrlicht <kori.irrlicht.dev@gmail.com>
2017-05-22 17:00:34 +0200

    Start with keyboard controller

--- core/input.go
+++ core/input.go
@@ -1,14 +1,40 @@
 package core
 
+// InputType is an abstract layer between controller/keyboard input and the input
+// the game will receive
 type InputType int
 
+// KeyboardMapping maps a key to an InputType
+type KeyboardMapping map[int]InputType
+
+// The InputTypes, which can be used
 const (
-	INPUT_UP InputType = iota
-	INPUT_DOWN
-	INPUT_LEFT
-	INPUT_RIGHT
+	InputUp InputType = iota
+	InputDown
+	InputLeft
+	InputRight
 )
 
+// Controller is an abstraction of the keyboard or a game controller (PS4, XBox1, Steam,...)
 type Controller interface {
 	IsDown(InputType) bool
+}
+
+// keyboardController is used, when the game is played with a keyboard
+type keyboardController struct {
+	keyState map[InputType]bool
+	mapping  KeyboardMapping
+}
+
+// IsDown implements the Controller interface
+func (k keyboardController) IsDown(it InputType) bool {
+	return false
+}
+
+// NewKeyboardController creates a new Controller with the given KeyboardMapping
+func NewKeyboardController(km KeyboardMapping) Controller {
+	c := keyboardController{}
+	c.keyState = make(map[InputType]bool)
+	c.mapping = km
+	return c
 }
